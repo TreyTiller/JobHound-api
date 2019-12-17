5101745d57d2235d4fc9e89309ec3405a18b95ab
Trey <treytiller@outlook.com>
2019-12-17 12:26:19 -0500

    Removed client origin on cors

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-const {CLIENT_ORIGIN} = require('./config')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 // const recipeRoutes = require('./recipe/routes')
@@ -20,7 +19,7 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors({origin: CLIENT_ORIGIN}));
+app.use(cors());
 
 // app.use('/api/auth', authRouter)
 // app.use('/api/recipes', recipeRoutes)
