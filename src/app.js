e196304ec05eda9fb442b9f7c73de72edd46cfdb
Trey <treytiller@outlook.com>
2019-12-17 11:30:59 -0500

    Cors intialized

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const {CLIENT_ORIGIN} = require('./config')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 const recipeRoutes = require('./recipe/routes')
@@ -19,7 +20,7 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
+app.use(cors({origin: CLIENT_ORIGIN}));
 
 app.use('/api/auth', authRouter)
 app.use('/api/recipes', recipeRoutes)
